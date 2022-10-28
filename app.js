6842778b62a202f950e9c3c2f6e45bb0c5657fcb
Wahab <mailwahab247@gmail.com>
2022-10-28 16:20:52 +0000

    connect server to db

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+
+// Package usage
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Hello World</h1>')
+})
+
+//routes
+
+
+const port = process.env.PORT || 8000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
